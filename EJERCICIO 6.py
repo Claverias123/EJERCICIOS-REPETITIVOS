418b0c7fc9670fa34df922ffab6dad035f3a7080
Claverias123 <al_amartinezc@iessierradelasvillas.org>
2022-10-31 11:08:03 +0100

    subir 6

---
+++ EJERCICIO 6.py
@@ -0,0 +1,15 @@
+'''
+Escribir un programa que imprima todos los números pares entre dos números
+que se le pidan al usuario.
+'''
+num=0
+num1=int(input("Dime un numero\n"))
+num2=int(input("Dime otro numero\n"))
+
+print("Los numeros pares son:")
+
+for num in range(num1,num2):
+    if num%2==0:
+        print(num)
+   
+        
