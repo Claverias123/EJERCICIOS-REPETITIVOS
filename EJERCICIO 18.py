e688ee0cc43aa47dd6148978534f14a5cd6a0987
Claverias123 <al_amartinezc@iessierradelasvillas.org>
2022-11-13 17:50:31 +0100

    subir 18

---
+++ EJERCICIO 18.py
@@ -0,0 +1,13 @@
+h=0
+m=0
+s=0
+d=0
+while h!=24:
+    h+=1
+    print(h,"horas")
+    while m!=60:
+        m +=1
+        print(m,"minutos")
+        while s!=60:
+            s +=0.0002
+            print(s,"segundos")
