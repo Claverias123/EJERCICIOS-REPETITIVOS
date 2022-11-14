9003ac21ab7546d904f2b8a1367afecf542ae743
Claverias123 <114221111+Claverias123@users.noreply.github.com>
2022-11-14 10:28:41 +0100

    Delete EJERCICIO 19.py

--- EJERCICIO 19.py
+++
@@ -1,8 +0,0 @@
-s=0
-s=int(input("Que quieres hacer:"))
-print("Pulsa un numero distinto de 1 si quieres continuar")
-print("Pulsa 1 si quieres salir")
-while s!=1:
-    s=int(input("Que quieres hacer:"))
-    print("Pulsa 1 si quieres salir")
-print("Has salido")
